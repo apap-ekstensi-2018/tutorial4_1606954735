a6379fb0eae015d138948aee0b2df3fffad92534
AzureAD\ArriKurniawan <arri.jtk11@gmail.com>
2018-03-07 00:12:19 +0700

    added tutorial4_1606954735 Project

---
+++ src/main/java/com/example/tutorial4_1606954735Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tutorial4_1606954735Application
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (tutorial4_1606954735Application.class, args);
+    }
+}
